49a8891d8eaae353d6d38ce7678a33242407ee21
cyx0706 <3519161997@qq.com>
2020-11-20 19:14:41 +0800

    feat: 初步完成了可以用的模型，准确率稍低

---
+++ README.md
@@ -0,0 +1,29 @@
+# iBird-Model
+
+这是中国海洋大学 2020 年软件工程课程设计 iBird 项目的模型代码仓库。
+
+用于实现一个 200 种类鸟类的分类，采用了 BCNN 模型。
+
+## 仓库构成
+
+```
+.
+├── README.md
+├── ibird.ipynb  # 在 colab 上运行的代码
+├── imagenet_class_index.json  # vgg16 迁移模型数据
+├── main.py
+├── model.py
+├── predict.py
+├── test_cuda.py
+└── trainer.py
+```
+
+## 使用说明
+
+执行 main.py 即可，依赖包括
+
+> tqdm
+> override
+> pytorch
+> torchvision
+> pandas
---
+++ predict.py
@@ -0,0 +1,51 @@
+from model import BilinearModel
+from trainer import Trainer
+from torchvision import transforms
+import torch
+import torch.nn as nn
+import os
+import torchvision
+import csv
+from main import data_dir
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Using gpu: %s" % torch.cuda.is_available())
+model = BilinearModel(num_classes=200)
+model.load("../results/checkpoint.pt")
+
+criterion = nn.CrossEntropyLoss()
+model.to(device)
+criterion.to(device)
+lr = 0.001
+optimizer = torch.optim.Adam(model.classifier.parameters(), lr=lr)
+
+trainer = Trainer(model, criterion, optimizer, device)
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+trans = transforms.Compose([
+    transforms.Resize((448, 448)),
+    # transforms.CenterCrop(448),
+    transforms.ToTensor(),
+    normalize,
+])
+predict_sets = torchvision.datasets.ImageFolder(os.path.join(data_dir, "test"), transform=trans)
+test_loader = torch.utils.data.DataLoader(
+    predict_sets,
+    batch_size=1,  # 一次一张一张的预测
+    shuffle=False,
+    num_workers=0
+)
+predictions = trainer.predict(test_loader)
+answer = []
+for index, cls in enumerate(predictions):
+    path = predict_sets.imgs[index][0]
+    path = path.split("/")
+    img_name = path[-1]
+    answer.append((img_name, int(predictions[index]) + 1))
+answer = sorted(answer, key=lambda x: int(x[0].split(".")[0]))
+
+with open('test.csv', 'w', newline="")as f:
+    writer = csv.writer(f)
+    for one in answer:
+        writer.writerow([one[0], one[1]])
